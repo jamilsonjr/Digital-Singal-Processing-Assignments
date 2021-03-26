daf439d8b06e27ce495f507da9822a03cf929dd1
jamilsonjr <jamilsonpinheiro.jr@gmail.com>
2021-03-26 14:23:21 +0000

    After end of class

--- Lab1/main.m
+++ Lab1/main.m
@@ -1,4 +1,6 @@
-clear
+clear 
+close all
+clc
 %% First assignment
 %% Create the continous time signal
 t = linspace(0, 2, 16000);
@@ -9,6 +11,7 @@ Omega = @(t) 2*pi*(k_2*t.^2 + k_1*t + F_0);
 %chirp
 x_c = @(t) cos( 2*pi*( (1/3)*k_2*t.^3 + (1/2)*k_1*t.^2 + F_0*t + phi_0) );
 x = x_c(t);
+figure
 plot(t, x);
 %% questions
 % Confirmar se esta implementação está certa?
@@ -21,13 +24,31 @@ sound(x, 8000);
 %%
 % From the script tens
 N=32; 
-% figure
+figure
 spectrogram(x, hann(N), 3*N/4, 4*N, 8000, 'yaxis')
 %% questions
 %% Problem 4
 [r, Fs] = audioread('romanza.wav');
 %%
-sound(r, Fs);
+soundsc(r, Fs);
+pause(5)
+clear sound
 %%
-figure
-spectrogram(r, hann(N), 3*N/4, 4*N, Fs, 'yaxis')
+N = 1024; 
+figure(88)
+spectrogram(r(1:15*Fs), hann(N), 3*N/4, 4*N, Fs, 'yaxis');
+%% Sampling Confirmar
+r_sampled = r(1:5:15*Fs);
+figure(99)
+spectrogram(r_sampled, hann(N), 3*N/4, 4*N, Fs/5, 'yaxis');
+% Camera lenta
+% soundsc(r_sampled, Fs/5);
+% pause(3)
+% clear sound
+%% Filtering
+r_filtered = filter(fir1(100, 0.2), 1, r);
+spectrogram(r_filtered(1:5:15*Fs), hann(N), 3*N/4, 4*N, Fs/5, 'yaxis');
+% soundsc(r_filtered, Fs);
+% pause(5)
+% clear sound
+
