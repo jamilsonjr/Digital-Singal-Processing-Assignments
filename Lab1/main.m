32e1960945b8b8cba2f4634d74e45a72c33c574d
jamilsonjr <jamilsonpinheiro.jr@gmail.com>
2021-03-26 11:55:58 +0000

    doing - implemented until question 4

--- Lab1/main.m
+++ Lab1/main.m
@@ -1,21 +1,33 @@
 clear
 %% First assignment
 %% Create the continous time signal
-t = linspace(0, 2, 100000);
+t = linspace(0, 2, 16000);
 % Constants
 [k_2, k_1, F_0, phi_0] = deal(1000, 0, 0, 0);
 % Instantaneous frrequency
 Omega = @(t) 2*pi*(k_2*t.^2 + k_1*t + F_0);
 %chirp
-x_c = @(Omega) cos(Omega);
-plot(t, x_c(Omega(t)))
-%% Create the continuous time impulse train
-
-w = 1/(2*800); %pulse width
-d = w/2:w*2:10; %delay vector
-pulse_train = pulstran(t, d, 'rectpuls', w); 
-plot(t,y2); 
-set(gca,'Ylim',[-0.1 1.1]);
-
-%% Product between
-
+x_c = @(t) cos( 2*pi*( (1/3)*k_2*t.^3 + (1/2)*k_1*t.^2 + F_0*t + phi_0) );
+x = x_c(t);
+plot(t, x);
+%% questions
+% Confirmar se esta implementação está certa?
+% Não existe aliasing pois não?
+% o t está bem?
+%%
+sound(x, 8000);
+%%
+% OK? Era o que estavamos à espepra
+%%
+% From the script tens
+N=32; 
+% figure
+spectrogram(x, hann(N), 3*N/4, 4*N, 8000, 'yaxis')
+%% questions
+%% Problem 4
+[r, Fs] = audioread('romanza.wav');
+%%
+sound(r, Fs);
+%%
+figure
+spectrogram(r, hann(N), 3*N/4, 4*N, Fs, 'yaxis')
