dfc6c386976c76632c17dd0237a7d25992c95d16
jamilsonjr <jamilsonpinheiro.jr@gmail.com>
2021-04-14 18:56:39 +0100

    with inidcations for pungo

--- Spectral_Analysis_of_Synthetic_and_Real_Signals/main.m
+++ Spectral_Analysis_of_Synthetic_and_Real_Signals/main.m
@@ -3,7 +3,9 @@ close all
 clear
 %% Lab2: Discrete Fourier Transform
 %% Spectral analysis of a synthetic signal.
-% Set M HERE!!!!
+% Olá Colega Pungo, não te esqueças de apagar os comments antes de enviar à
+% professora! Thanks 
+% Set M HERE!!!! First to 512 then to 1024
 M = 1024;
 % Build the signal
 w0 = @(M) 5.2*2*pi/M;
@@ -12,6 +14,7 @@ n = linspace(0,M-1, M);
 x = x_(n);
 %%
 spectral_analysis(x, M);
+% R1F) PUNGO: Repeat everything with M = 1024
 %% Spectral analysis of a real voice signal
 %% R2a)
 [r, Fs] = audioread('How_many_roads.wav');
@@ -21,7 +24,7 @@ M = 2048;
 new_signal = r(48500:48500+M-1, :);
 %% R2c)
 dft = spectral_analysis(new_signal, M); %% Doent no work well! Why?
-figure()
-plot(0:M-1, ifft(dft))
+% Make the same comments as those asked in the first part.
 %%
+% Todo: last question 
 
