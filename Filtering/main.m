bb65ec1335bab57b3bc4145c57b63106cc3effab
jamilsonjr <jamilsonpinheiro.jr@gmail.com>
2021-05-06 15:02:31 +0100

    until question2

---
+++ Filtering/main.m
@@ -0,0 +1,22 @@
+%% Filtering 
+%% R1a) Listen to the sound
+% Pungo comment we hear the music with noise, like tik tiks. The msuic is a
+% fucking classic! Love it! 
+[y, Fs] = audioread('fugee.wav');
+% sound(y, Fs);
+%% R1b)Plot the Signal
+% Pungo comment that the noise is in the form of diracs and we see them in
+% the plot
+figure()
+plot(y);
+%% R1c) Visualise Signal’s Magnitude Spectrum
+% Use logarithmic scale because the sensitivity of our hearing is
+% logarithmic as well.
+Y = fft(y);
+figure()
+semilogy(abs(fftshift(Y))) 
+grid on
+%% LTI filter
+%% R2a)
+%[b,a] = butter(10, ) What is the cuttof frequency? 
+
