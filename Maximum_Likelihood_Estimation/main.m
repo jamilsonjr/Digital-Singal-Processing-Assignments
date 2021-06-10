2c133503ffb368d9cee35935875821da04c37014
jamilsonjr <jamilsonpinheiro.jr@gmail.com>
2021-06-10 11:24:59 +0100

    força pungo

--- Maximum_Likelihood_Estimation/main.m
+++ Maximum_Likelihood_Estimation/main.m
@@ -15,5 +15,18 @@ image_croped_water= imcrop(imagem);
 figure; 
 imagesc(image_croped_water);
 
-% Estimate the parameters of the distributions:
-% Exponential
+% Estimate the parameters of the distributions: PUNGO Comenta esta parte sff
+% Mean
+ice_mean = mean(mean(image_croped_ice)); % The first mean return mean of each column 
+water_mean = mean(mean(image_croped_water));
+
+% b 
+ice_b = mean(mean(image_croped_ice.^2))/2;
+water_b = mean(mean(image_croped_ice.^2))/2;
+
+%Variance
+ice_variance = mean(mean((image_croped_ice-ice_mean).^2));
+water_variance = mean(mean((image_croped_water-water_mean).^2));
+
+% PUNGO: OBTER OS RESULTADOS PARA AS PERGUNTAS
+%% R1d) Plot the distributions and Compare with the 
